d31920e2a572f297144bee2d369c74b27b89337b
Prasanna Sridhar <samthedevil.sp@gmail.com>
2023-03-02 00:42:41 +0000

    Add  ooption to allow users to pass a directory different from default

--- src/projects.py
+++ src/projects.py
@@ -1,6 +1,7 @@
 import itertools
 import re
 from pathlib import Path
+import shutil
 from typing import Optional, Literal
 
 path_replace_pattern = re.compile(r"[^\w\-_\. ]")
@@ -53,7 +54,7 @@ def get_wise_thumbs_dataset_path(project_id: str) -> Path:
     return get_wise_project_folder(project_id) / "thumbs" / "thumbs-%05d.h5"
 
 
-def create_wise_project_tree(project_id: str):
+def create_wise_project_tree(project_id: str, destination_dir: Optional[Path] = None):
 
     # Create folder structure to hold the data
     # - features/
@@ -66,6 +67,19 @@ def create_wise_project_tree(project_id: str):
     # - thumbs/
     #   - thumbs-%05d.h5
     base_dir = get_wise_project_folder(project_id)
+
+    if destination_dir:
+        # $HOME/.wise/projects/{PROJECT_ID} -> /path/to/destination/{PROJECT_ID}
+        project_dir = (destination_dir / project_id).resolve()
+        if project_dir.is_dir():
+            shutil.rmtree(project_dir)
+        project_dir.mkdir()
+        warning_file = project_dir / "DANGER.txt"
+        warning_file.write_text(
+            "DO NOT STORE ANYTHING IMPORTANT HERE\n\nMANAGED BY WISE CLI.\n\nCONTENTS MIGHT BE DELETED!",
+            encoding="utf-8",
+        )
+        base_dir.symlink_to(project_dir, target_is_directory=True)
 
     # Make the thumbs dir
     thumbs_dir = base_dir / "thumbs"
