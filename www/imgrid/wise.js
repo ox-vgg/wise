c64d0c4879af579ddbca5d22eb184ea0c10e5ed6
Horace Lee <horacelee18ic@gmail.com>
2023-04-25 19:39:00 +0100

    Show Wikimedia featured images

--- www/imgrid/wise.js
+++ www/imgrid/wise.js
@@ -25,9 +25,9 @@ const UI_MODE = {
 }
 
 // state
-var wise_home_rand_img_index_list = [];
-var wise_home_from_findex = -1;
-var wise_home_to_findex = -1;
+var wise_home_featured_images = [];
+var wise_home_from_findex = 0;
+var wise_home_to_findex = PAGE_IMG_COUNT;
 var wise_result_start_findex = -1;
 var wise_result_end_findex = -1;
 var wise_current_ui_mode = UI_MODE.BROWSE_IMAGES;
@@ -42,13 +42,12 @@ let search_query = {
 //
 const TOOLBAR_INFO = 'Try searching, for example, using <span class="text_button" onclick="search_for(\'bees feeding on flower\')">bees feeding on flower</span> or <span class="text_button" onclick="search_for(\'car on empty street in snow\')">car on empty street in snow</span> or <span class="text_button" onclick="search_for(\'horse in river\')">horse in river</span>.'
 function init_home_page() {
-	load_featured_image_grid();
+	load_project_info();
+	load_featured_images();
 	document.getElementById('toolbar').innerHTML = TOOLBAR_INFO;
 }
 
-function load_featured_image_grid() {
-	wise_current_ui_mode = UI_MODE.BROWSE_IMAGES;
-
+function load_project_info() {
 	fetch("info", {
 		method: 'GET'
 	})
@@ -59,24 +58,24 @@ function load_featured_image_grid() {
 			// initialise the toolbar with info
 			const img_count_percent = ((wise_data['info']['num_images'] / 80000000) * 100).toFixed(0);
 			document.getElementById('toolbar').innerHTML = 'Here, you can search nearly ' + img_count_percent + '% of the 80 million images in Wikimedia Commons.' + TOOLBAR_INFO;
+		});
+}
 
-			// get total page count
-			navinfo1.innerHTML = 'Showing selected images.';
-			navinfo2.innerHTML = 'Use arrow keys (or buttons) to navigate ' + wise_data['info']['num_images'].toLocaleString('en', { useGrouping: true }) + ' images.';
-			imgrid_container.innerHTML = '';
-			wise_home_rand_img_index_list = [];
-			for (var i = 0; i < PAGE_IMG_COUNT; ++i) {
-				rand_img_index = get_rand_int(0, wise_data['info']['num_images']);
-				const img = document.createElement('img');
-				img.src = 'thumbs/' + rand_img_index;
-				const a = document.createElement('a');
-				a.setAttribute('href', 'images/' + rand_img_index);
-				a.setAttribute('target', '_blank');
-				a.appendChild(img);
+function load_featured_images() {
+	wise_current_ui_mode = UI_MODE.BROWSE_IMAGES;
 
-				wise_home_rand_img_index_list.push(rand_img_index)
-				imgrid_container.appendChild(a);
-			}
+	fetch("featured_images.json", {
+		method: 'GET'
+	})
+		.then((response) => response.json())
+		.then((featured_images_json) => {
+			wise_home_featured_images = featured_images_json
+				.map(value => ({ value, sort: Math.random() }))
+				.sort((a, b) => a.sort - b.sort)
+				.map(({ value }) => value); // Shuffle images
+		
+			wise_data['num_featured_images'] = wise_home_featured_images.length;
+			show_featured_images(wise_home_from_findex, wise_home_to_findex);
 		});
 }
 
@@ -89,18 +88,13 @@ function get_rand_int(min, max) {
 
 function show_next_page() {
 	if (wise_current_ui_mode === UI_MODE.BROWSE_IMAGES) {
-		if (wise_home_from_findex === -1 && wise_home_to_findex === -1) {
-			wise_home_from_findex = 0;
-			wise_home_to_findex = PAGE_IMG_COUNT;
-		} else {
-			wise_home_from_findex = wise_home_to_findex;
-			wise_home_to_findex = wise_home_to_findex + PAGE_IMG_COUNT;
-		}
-		if (wise_home_to_findex > wise_data['info']['num_images']) {
+		wise_home_from_findex = wise_home_to_findex;
+		wise_home_to_findex = wise_home_to_findex + PAGE_IMG_COUNT;
+		if (wise_home_to_findex > wise_data['num_featured_images']) {
 			wise_home_from_findex = 0;
 			wise_home_to_findex = PAGE_IMG_COUNT;
 		}
-		show_images(wise_home_from_findex, wise_home_to_findex)
+		show_featured_images(wise_home_from_findex, wise_home_to_findex)
 	} else {
 		if (wise_result_start_findex < MAX_SEARCH_RESULT && wise_result_end_findex < MAX_SEARCH_RESULT) {
 			wise_result_start_findex = wise_result_end_findex;
@@ -117,18 +111,13 @@ function show_next_page() {
 
 function show_prev_page() {
 	if (wise_current_ui_mode === UI_MODE.BROWSE_IMAGES) {
-		if (wise_home_from_findex === -1 && wise_home_to_findex === -1) {
-			wise_home_to_findex = wise_data['info']['num_images'];
-			wise_home_from_findex = wise_data['info']['num_images'] - PAGE_IMG_COUNT;
-		} else {
-			wise_home_to_findex = wise_home_from_findex;
-			wise_home_from_findex = wise_home_from_findex - PAGE_IMG_COUNT;
-		}
+		wise_home_to_findex = wise_home_from_findex;
+		wise_home_from_findex = wise_home_from_findex - PAGE_IMG_COUNT;
 		if (wise_home_from_findex < 0) {
-			wise_home_from_findex = wise_data['info']['num_images'] - PAGE_IMG_COUNT;
-			wise_home_to_findex = wise_data['info']['num_images'];
+			wise_home_from_findex = wise_data['num_featured_images'] - PAGE_IMG_COUNT;
+			wise_home_to_findex = wise_data['num_featured_images'];
 		}
-		show_images(wise_home_from_findex, wise_home_to_findex)
+		show_featured_images(wise_home_from_findex, wise_home_to_findex)
 	} else {
 		if (wise_result_start_findex > 0) {
 			wise_result_end_findex = wise_result_start_findex;
@@ -143,20 +132,24 @@ function show_prev_page() {
 	}
 }
 
-function show_images(from_findex, to_findex) {
-	navinfo1.innerHTML = 'Showing images';
+function show_featured_images(from_findex, to_findex) {
+	navinfo1.innerHTML = 'Showing featured images';
 	imgrid_container.innerHTML = '';
 	for (var i = from_findex; i < to_findex; ++i) {
+		const img_link = wise_home_featured_images[i]['original_download_url']
+		const img_link_tok = img_link.split('/');
+		const img_filename = img_link_tok[img_link_tok.length - 2];
+
 		const img = document.createElement('img');
-		img.src = 'thumbs/' + i;
+		img.src = img_link;
 		const a = document.createElement('a');
-		a.setAttribute('href', 'images/' + i);
+		a.setAttribute('href', 'https://commons.wikimedia.org/wiki/File:' + img_filename);
 		a.setAttribute('target', '_blank');
 		a.appendChild(img);
 
 		imgrid_container.appendChild(a);
 	}
-	navinfo2.innerHTML = 'from ' + (from_findex + 1) + ' to ' + to_findex + ' of ' + wise_data['info']['num_images'].toLocaleString('en', { useGrouping: true }) + ' images.'
+	navinfo2.innerHTML = (from_findex + 1) + ' to ' + to_findex;
 }
 
 //
