1875defc9e075f436d0edf1443d30c7d7134c59f
Horace Lee <horacelee18ic@gmail.com>
2023-04-25 20:20:46 +0100

    Remove unused function

--- www/imgrid/wise.js
+++ www/imgrid/wise.js
@@ -79,13 +79,6 @@ function load_featured_images() {
 		});
 }
 
-// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function get_rand_int(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-}
-
 function show_next_page() {
 	if (wise_current_ui_mode === UI_MODE.BROWSE_IMAGES) {
 		wise_home_from_findex = wise_home_to_findex;
