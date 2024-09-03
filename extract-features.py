ox-vgg/wise

commit a25a334629b45baff4d2b7f38395a9f50e14f5b3

Refactor wise2 to allow image ingestion

extract-features.py
@@ -1,18 +1,15 @@
 import argparse
-import glob
 import os
 import time
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, Literal
 import torch.utils.data as torch_data
 from tqdm import tqdm
 import numpy as np
-from collections import defaultdict
 import logging
 
 from src.dataloader.dataset import MediaChunk
-from src.dataloader import MediaDataset, AVDataset, VideoDataset, AudioDataset, ImageDataset, get_media_metadata
-from src.dataloader.utils import VIDEO_EXTENSIONS, is_valid_image, is_valid_video
+from src.dataloader import  get_dataset, get_metadata_for_valid_files, DatasetPayload
 from src.dataloader.streamreader import MediaChunkType
 from src.wise_project import WiseProject
 from src.feature.feature_extractor_factory import FeatureExtractorFactory
@@ -35,6 +32,101 @@ from src.repository import (
     ThumbnailRepo,
 )
 
+
+def get_files_from_directory_with_extensions(dir: Path, extensions: list[str]):
+    return (x for ext in extensions for x in dir.rglob(ext) if x.is_file())
+
+def initialise_feature_extractors(
+        project: WiseProject,
+        feature_extractor_ids: dict[Literal['video', 'audio', 'image'], str],
+        feature_store_type: Literal['webdataset', 'numpy'],
+        shard_max_count: int,
+        shard_max_size: int):
+    ## 3. Prepare for feature extraction and storage
+    logger.info(f"Initialising feature extractor")
+
+    feature_extractors = {}
+    feature_stores = {}
+
+    for media_type, feature_extractor_id in feature_extractor_ids.items():
+        ## 3.1 Initialise feature extractor
+        feature_extractors[media_type] = FeatureExtractorFactory(
+            feature_extractor_id
+        )
+        print(f"Using {feature_extractor_id} for {media_type}")
+
+        ## 3.2 Create folders to store features, metadata and search index
+        project.create_features_dir(feature_extractor_id)
+
+        ## 3.3 Initialise feature store to store features
+        feature_stores[media_type] = FeatureStoreFactory.create_store(
+            feature_store_type,
+            media_type,
+            project.features_dir(feature_extractor_id),
+        )
+        feature_stores[media_type].enable_write(
+            shard_max_count, shard_max_size
+        )
+
+    return feature_extractors, feature_stores
+
+def process_media_dir(media_dir: Path, db_engine, include_extensions: list[str] = ['*']):
+
+    # Get files matching extensions
+    input_files = list(get_files_from_directory_with_extensions(media_dir, include_extensions))
+
+    # Get metadata and media datasets corresponding to the files
+    metadata, unknown_files = get_metadata_for_valid_files(input_files)
+    if len(unknown_files) > 0:
+        logger.info(f'Skipping {len(unknown_files)} files that are not valid media in directory "{media_dir}"')
+        logger.debug("\n".join(map(str, unknown_files)))
+
+    # Add metadata to database
+    dataset_payload: list[DatasetPayload] = []
+    with tqdm(total=len(metadata)) as pbar, db_engine.begin() as conn:
+        # Add each folder to source collection table
+        data = SourceCollection(
+            location=str(media_dir), type=SourceCollectionType.DIR
+        )
+        media_source_collection = SourceCollectionRepo.create(conn, data=data)
+
+        for media_metadata in metadata:
+            # Get metadata for each file and add it to media table
+            # Get media_path relative to
+            media_path = media_metadata.path
+            _metadata = MediaRepo.create(
+                conn,
+                data=MediaMetadata(
+                    source_collection_id=media_source_collection.id,
+                    path=os.path.relpath(
+                        media_path, media_source_collection.location
+                    ),
+                    media_type=media_metadata.media_type,
+                    checksum=media_metadata.md5sum,
+                    size_in_bytes=os.path.getsize(media_path),
+                    date_modified=os.path.getmtime(media_path),
+                    format=media_metadata.format,
+                    width=media_metadata.width,
+                    height=media_metadata.height,
+                    num_frames=media_metadata.num_frames,
+                    duration=media_metadata.duration or 0,
+                ),
+            )
+            # extra_metadata = ExtraMediaMetadata(
+            #     media_id=_metadata.id,
+            #     metadata={
+            #         "fps": media_metadata.fps,
+            #     }
+            #     | media_metadata.extra,
+            # )
+            # MediaMetadataRepo.create(conn, data=extra_metadata)
+            dataset_payload.append(DatasetPayload(_metadata.id, media_path, _metadata.media_type))
+            pbar.update(1)
+
+
+    # return metadata and datasets to be chained
+    return dataset_payload
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="extract-features",
@@ -42,12 +134,9 @@ if __name__ == "__main__":
         epilog="For more details about WISE, visit https://www.robots.ox.ac.uk/~vgg/software/wise/",
     )
     parser.add_argument(
-        "--media-dir",
-        required=True,
-        action="append",
-        dest="media_dir_list",
-        type=str,
-        help="source images and video from this folder",
+        "media_dir_list",
+        nargs='+',
+        help="process images and video from this folder",
     )
 
     parser.add_argument(
@@ -55,7 +144,7 @@ if __name__ == "__main__":
         required=False,
         action="append",
         dest="media_include_list",
-        default=["*.jpg"],
+        default=[],
         type=str,
         help="regular expression to include certain media files",
     )
@@ -110,12 +199,14 @@ if __name__ == "__main__":
         help="use this feature extractor for audio samples",
     )
 
-    parser.add_argument(
-        "--skip-audio-feature-extraction",
-        required=False,
-        action="store_true",
-        help="skip the extraction of audio features (for videos)"
-    )
+    # TODO: Temporarily disabling this feature
+    # Need to implement it as a parameter in the get_dataset call
+    # parser.add_argument(
+    #     "--skip-audio-feature-extraction",
+    #     required=False,
+    #     action="store_true",
+    #     help="skip the extraction of audio features (for videos)"
+    # )
 
     parser.add_argument(
         "--project-dir",
@@ -136,12 +227,18 @@ if __name__ == "__main__":
     logger = logging.getLogger()
 
     # sanity check: remove duplicate entries in command line args
-    if len(args.media_include_list) > 1:
+    n_extension = len(args.media_include_list)
+    if n_extension == 0:
+        setattr(args, 'media_include_list', ['*'])
+    else:
         unique_media_include_list = list(set(args.media_include_list))
         setattr(args, 'media_include_list', unique_media_include_list)
+
     if len(args.media_dir_list) > 1:
         unique_media_dir_list = list(set(args.media_dir_list))
         setattr(args, 'media_dir_list', unique_media_dir_list)
+
+    assert all(Path(x).is_dir() for x in args.media_dir_list), "All values for media_dir_list must be directories"
 
     # we need a non-existing folder to initialise a new WISE project
     if Path(args.project_dir).exists():
@@ -154,99 +251,29 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    ## 1. Check validity of input files
-    print("Checking for invalid media files")
-    # Create dictionary of media files; key: media_dir, value: list of filepaths
-    valid_media_files: defaultdict[str, List[str]] = defaultdict(list)
-    for media_dir in args.media_dir_list:
-        for media_include in args.media_include_list:
-            media_search_dir = os.path.join(media_dir, "**/" + media_include)
-            media_paths = list(glob.iglob(pathname=media_search_dir, recursive=True))
-            media_paths = [Path(x) for x in media_paths]
-            media_paths = [str(x) for x in tqdm(media_paths) if x.is_file() and is_valid_image(x)]
-            if len(media_paths) > 0:
-                valid_media_files[media_dir] += media_paths
-
-    if len(valid_media_files) == 0:
-        raise ValueError("No valid media files found")
-
-    ## 2. Initialise internal metadata database
+    ## 1. Initialise internal metadata database with valid files
     print('Initialising internal metadata database')
-    media_filelist: Dict[int, str] = {} # maps media ids to filepaths
-    with tqdm(total=sum(len(media_paths) for media_paths in valid_media_files.values())) as pbar:
-        for media_dir, media_paths in valid_media_files.items():
-            with db_engine.begin() as conn:
-                # Add each folder to source collection table
-                data = SourceCollection(
-                    location=media_dir, type=SourceCollectionType.DIR
-                )
-                media_source_collection = SourceCollectionRepo.create(conn, data=data)
-            
-                for media_path in media_paths:
-                    # Get metadata for each file and add it to media table
-                    # Get media_path relative to
-                    media_metadata = get_media_metadata(media_path)
-                    metadata = MediaRepo.create(
-                        conn,
-                        data=MediaMetadata(
-                            source_collection_id=media_source_collection.id,
-                            path=os.path.relpath(
-                                media_path, media_source_collection.location
-                            ),
-                            media_type=media_metadata.media_type,
-                            checksum=media_metadata.md5sum,
-                            size_in_bytes=os.path.getsize(media_path),
-                            date_modified=os.path.getmtime(media_path),
-                            format=media_metadata.format,
-                            width=media_metadata.width,
-                            height=media_metadata.height,
-                            num_frames=media_metadata.num_frames,
-                            duration=media_metadata.duration,
-                        ),
-                    )
-                    # extra_metadata = ExtraMediaMetadata(
-                    #     media_id=metadata.id,
-                    #     metadata={
-                    #         "fps": media_metadata.fps,
-                    #     }
-                    #     | media_metadata.extra,
-                    # )
-                    # MediaMetadataRepo.create(conn, data=extra_metadata)
-                    media_filelist[metadata.id] = media_path
-                    pbar.update(1)
+    all_metadata = []
+    for media_dir in args.media_dir_list:
+        metadata = process_media_dir(Path(media_dir), db_engine, args.media_include_list)
+        all_metadata.extend(metadata)
 
-    ## 3. Prepare for feature extraction and storage
-    print(f"Extracting features from {len(media_filelist)} files")
-    feature_extractor_id_list = {}
-    feature_extractor_id_list['video'] = args.video_feature_id
-    if not args.skip_audio_feature_extraction:
-        feature_extractor_id_list['audio'] = args.audio_feature_id
 
-    feature_extractor_list = {}
-    feature_store_dir_list = {}
-    feature_store_list = {}
+    ## 5. extract video and audio features
+    feature_extractor_ids = {}
+    feature_extractor_ids['video'] = args.video_feature_id
+    feature_extractor_ids['image'] = args.video_feature_id
+    # TODO: temporary disable - if not args.skip_audio_feature_extraction:
+    feature_extractor_ids['audio'] = args.audio_feature_id
 
-    for media_type, feature_extractor_id in feature_extractor_id_list.items():
-        ## 3.1 Initialise feature extractor
-        feature_extractor_list[media_type] = FeatureExtractorFactory(
-            feature_extractor_id
-        )
-        print(f"Using {feature_extractor_id} for {media_type}")
+    feature_extractors, feature_stores = initialise_feature_extractors(
+        project,
+        feature_extractor_ids,
+        args.feature_store_type,
+        args.shard_maxcount, args.shard_maxsize,
+    )
 
-        ## 3.2 Create folders to store features, metadata and search index
-        project.create_features_dir(feature_extractor_id)
-
-        ## 3.3 Initialise feature store to store features
-        feature_store_list[media_type] = FeatureStoreFactory.create_store(
-            args.feature_store_type,
-            media_type,
-            project.features_dir(feature_extractor_id),
-        )
-        feature_store_list[media_type].enable_write(
-            args.shard_maxcount, args.shard_maxsize
-        )
-
-    ## 4. Initialise data loader
+    ## dataset
     audio_sampling_rate = 48_000  # (48 kHz)
     video_frame_rate = 2  # fps
     video_frames_per_chunk = 8  # frames
@@ -254,36 +281,23 @@ if __name__ == "__main__":
     audio_segment_length = segment_length  # seconds
     audio_frames_per_chunk = int(round(audio_sampling_rate * audio_segment_length))
 
-    stream: MediaDataset
-    if not args.skip_audio_feature_extraction:
-        stream = AVDataset(
-            media_filelist,
-            video_frames_per_chunk=video_frames_per_chunk,
-            video_frame_rate=video_frame_rate,
-            video_preprocessing_function=feature_extractor_list["video"].preprocess_image,
-            audio_samples_per_chunk=audio_frames_per_chunk,
-            audio_sample_rate=audio_sampling_rate,
-            audio_preprocessing_function=feature_extractor_list["audio"].preprocess_audio,
-            offset=None,
-            thumbnails=args.thumbnails,
-        )
-    else:
-        # stream = VideoDataset(
-        #     media_filelist,
-        #     frames_per_chunk=video_frames_per_chunk,
-        #     frame_rate=video_frame_rate,
-        #     preprocessing_function=feature_extractor_list["video"].preprocess_image,
-        #     offset=None,
-        #     thumbnails=args.thumbnails,
-        # )
-        stream = ImageDataset(
-            media_filelist,
-            preprocessing_function=feature_extractor_list["video"].preprocess_image,
-            thumbnails=args.thumbnails,
-        )
+    params = {
+        "video_frames_per_chunk": video_frames_per_chunk,
+        "video_frame_rate": video_frame_rate,
+        "video_preprocessing_function": feature_extractors['video'].preprocess_image,
 
+        "audio_samples_per_chunk": audio_frames_per_chunk,
+        "audio_sampling_rate": audio_sampling_rate,
+        "audio_preprocessing_function": feature_extractors['audio'].preprocess_audio,
 
-    ## 5. extract video and audio features
+        "image_preprocessing_function": feature_extractors['video'].preprocess_image,
+
+        "offset": None,
+        "thumbnails": args.thumbnails
+    }
+    stream = torch_data.ChainDataset(
+        get_dataset(all_metadata, params)
+    )
     print(f"Initializing data loader with {args.num_workers} workers ...")
     av_data_loader = torch_data.DataLoader(
         stream, batch_size=None, num_workers=args.num_workers
@@ -293,21 +307,21 @@ if __name__ == "__main__":
         mid: str | int # type annotation
         chunks: Dict[MediaChunkType, MediaChunk | None] # type annotation
         for idx, (mid, chunks) in enumerate(av_data_loader):
-            for media_type in feature_extractor_id_list:
-                if chunks[media_type] is None:
+            for media_type in feature_extractor_ids:
+                if media_type not in chunks:
                     continue
                 segment_tensor = chunks[media_type].tensor
                 segment_pts = chunks[media_type].pts
 
                 if media_type == "image" or media_type == "video":
-                    segment_feature = feature_extractor_list[
+                    segment_feature = feature_extractors[
                         media_type
                     ].extract_image_features(segment_tensor)
                 elif media_type == "audio":
                     if segment_tensor.shape[2] < audio_frames_per_chunk:
                         # we discard any malformed audio segments
                         continue
-                    segment_feature = feature_extractor_list[
+                    segment_feature = feature_extractors[
                         media_type
                     ].extract_audio_features(segment_tensor)
                 else:
@@ -324,7 +338,7 @@ if __name__ == "__main__":
                                 timestamp=segment_pts + i * (1 / video_frame_rate),
                             ),
                         )
-                        feature_store_list[media_type].add(
+                        feature_stores[media_type].add(
                             feature_metadata.id,
                             np.expand_dims(segment_feature[i], axis=0),
                         )
@@ -341,7 +355,7 @@ if __name__ == "__main__":
                             end_timestamp=_end_time,
                         ),
                     )
-                    feature_store_list[media_type].add(
+                    feature_stores[media_type].add(
                         feature_metadata.id, segment_feature
                     )
 
@@ -364,7 +378,7 @@ if __name__ == "__main__":
                     )
 
             # Update progress bar
-            _media = chunks.get('video') or chunks.get('audio')
+            _media = chunks.get('video') or chunks.get('audio') or chunks.get('image')
             if _media is not None:
                 pbar.update(_media.tensor.shape[0])
 
@@ -374,6 +388,10 @@ if __name__ == "__main__":
 
         conn.commit()
         thumbs_conn.commit()
+
+    for id in feature_stores:
+        store = feature_stores[id]
+        store.close()
 
     end_time = time.time()
     elapsed_time = end_time - start_time

create-index.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--media-type',
                         required=False,
                         action='append',
-                        choices=['audio', 'video', 'metadata'],
+                        choices=['audio', 'video', 'metadata', 'image'],
                         help='create search index only for these media types; applies to all by default ')
 
     parser.add_argument('--index-type',

environment.yml
@@ -14,6 +14,7 @@ dependencies:
   - torchvision=0.17.2
   - torchaudio=2.2.2
   - faiss-gpu=1.8.0
+  - python-magic=0.4
   - mkl=2023
   - blas=*=mkl
   - conda-forge::ffmpeg

search.py
@@ -255,9 +255,13 @@ def merge0(result, args):
         assert len(result[query_index]['in']) == 1, f'unexpected {result[query_index]["in"]}'
 
         media_type = result[query_index]['in'][0]
-        merge_tolerance_id = 'merge_tolerance_' + media_type
-        time_tolerance = getattr(args, merge_tolerance_id)
-        rank_tolerance = getattr(args, 'merge_rank_tolerance')
+        if media_type == 'image':
+            time_tolerance = 0
+            rank_tolerance = 0
+        else:
+            merge_tolerance_id = 'merge_tolerance_' + media_type
+            time_tolerance = getattr(args, merge_tolerance_id)
+            rank_tolerance = getattr(args, 'merge_rank_tolerance')
 
         filename_list = result[query_index]['match_filename_list']
         pts_list = result[query_index]['match_pts_list']
@@ -334,7 +338,7 @@ def merge_a_ranked_result_list(filename_list, pts_list, score_list, pts_toleranc
                 mid2 = sum(pts_list[pts_index2]) / len(pts_list[pts_index2])
                 del_pts = math.fabs(mid1 - mid2)
             else:
-                # if pts is a timestamp, compute their separation using their difference 
+                # if pts is a timestamp, compute their separation using their difference
                 del_pts = math.fabs(pts_list[pts_index1] - pts_list[pts_index2])
 
             if del_pts <= pts_tolerance:
@@ -677,14 +681,14 @@ if __name__ == '__main__':
                         required=False,
                         action='append',
                         dest='media_type_list', # since "in" is a reserved keyword
-                        choices=['audio', 'video', 'metadata'],
+                        choices=['audio', 'video', 'metadata', "image"],
                         help='apply the search query term to these features; query applied to all features if --in argument is missing')
 
     parser.add_argument('--not-in',
                         required=False,
                         action='append',
                         dest='media_type_not_list',
-                        choices=['audio', 'video', 'metadata'],
+                        choices=['audio', 'video', 'metadata', "image"],
                         help='remove the results from the preceeding query obtained in this media_type; Note: all --not-in flags must come after --in flags')
 
     parser.add_argument('--index-type',
@@ -727,6 +731,7 @@ if __name__ == '__main__':
                         type=int,
                         default=8,
                         help='tolerance (in seconds) for merging audio based search results')
+
 
     parser.add_argument('--merge-tolerance-metadata',
                         required=False,

src/dataloader/__init__.py
@@ -1,7 +1,10 @@
 from .dataset import (
     MediaChunk,
     MediaMetadata,
+    DatasetPayload,
     get_media_metadata,
+    get_dataset,
+    get_metadata_for_valid_files,
     ImageDataset,
     AudioDataset,
     VideoDataset,

src/dataloader/__main__.py
@@ -1,7 +1,7 @@
 import enum
 import logging
 from pathlib import Path
-from .dataset import get_media_metadata, AVDataset
+from .dataset import get_metadata_for_valid_files, get_dataset
 import numpy as np
 import torch
 import torch.utils.data as torch_data
@@ -9,6 +9,7 @@ import torchvision.transforms.v2 as transforms_v2
 import open_clip
 import typer
 from tqdm import tqdm
+from typing import List
 
 app = typer.Typer()
 app_state = {"verbose": True}
@@ -84,7 +85,7 @@ def base(verbose: bool = False):
 
 @app.command()
 def run(
-    input_file: Path = typer.Argument(
+    input_files: List[Path] = typer.Argument(
         ...,
         file_okay=True,
         dir_okay=True,
@@ -93,7 +94,7 @@ def run(
         help="Path to input file / folder of videos",
     ),
     model: CLIPModel = typer.Option(
-        ..., help="Pass in a open_clip model string (or) internvideo"
+       "ViT-B-32:openai", help="Pass in a open_clip model string (or) internvideo"
     ),
     thumbnails: bool = typer.Option(True, help="Flag to control thumbnail extraction"),
 ):
@@ -111,29 +112,10 @@ def run(
     python3 -m src run data/ --model "internvideo"
     """
 
-    logger.debug("Getting preprocessing function")
-    # get preprocessing function from feature extractor
-    preprocess = get_input_transform_for_model(model)
-
-    # Dummy filtering, will be replaced in the main code
-    video_extensions = (".mkv", ".mp4", ".webm")
-    if input_file.is_file() and input_file.suffix in video_extensions:
-        logger.debug(f'"{input_file}"is a file, adding to sources')
-        sources = [input_file]
-    elif input_file.is_dir():
-        logger.debug(f'"{input_file}"is a dir, globbing for videos')
-        sources = list(x for y in video_extensions for x in input_file.rglob(f"*{y}"))
-    else:
-        raise ValueError(f'Unrecognized file "{input_file}"')
-
-    # Get metadata to write into the media table
-    metadata = (get_media_metadata(str(x)) for x in sources)
-    input_files = {x.id: y for x, y in zip(metadata, sources)}
 
     # Define output stream options based on model.
     # Every 0.5 seconds, we read 8 frames chunk for internvideo, and 1 for clip
 
-    # Construct the dataset
     audio_sampling_rate = 48_000  # (48 kHz)
 
     video_frame_rate = 2  # fps
@@ -145,21 +127,32 @@ def run(
         round(audio_sampling_rate * segment_length)
     )  # audio frames spanning the same segment length as video
 
-    stream = AVDataset(
-        input_files,
-        video_frames_per_chunk=video_frames_per_chunk,
-        video_frame_rate=video_frame_rate,
-        video_preprocessing_function=preprocess,
-        audio_samples_per_chunk=audio_frames_per_chunk,
-        audio_sample_rate=audio_sampling_rate,
-        audio_preprocessing_function=None,
-        offset=None,
-        thumbnails=thumbnails,
-    )
+    # get preprocessing function from feature extractor
+    logger.debug("Getting preprocessing function")
+    preprocess = get_input_transform_for_model(model)
 
+    params = {
+        "video_frames_per_chunk": video_frames_per_chunk,
+        "video_frame_rate": video_frame_rate,
+        "video_preprocessing_function": preprocess,
+
+        "audio_samples_per_chunk": audio_frames_per_chunk,
+        "audio_sampling_rate": audio_sampling_rate,
+        "audio_preprocessing_function": None,
+
+        "image_preprocessing_function": preprocess,
+
+        "offset": None,
+        "thumbnails": thumbnails
+    }
+
+
+    # Get metadata to write into the media table
+    metadata, _ = get_metadata_for_valid_files(input_files)
+    stream = torch_data.ChainDataset(get_dataset(metadata, params))
     # Construct the dataloader
     loader = torch_data.DataLoader(stream, batch_size=None, num_workers=0)
-    logger.info(f"Iterating over {len(input_files)} file(s)")
+    logger.info(f"Iterating over {len(metadata)} file(s)")
     for mid, chunks in tqdm(loader):
         logger.debug([{
             media_chunk_type: (

commit d6e476a6a47893d53a6294687978440cd0b4b788

extract-features.py: fix bug introduced in a25a334

extract-features.py
@@ -323,7 +323,7 @@ if __name__ == "__main__":
         chunks: Dict[MediaChunkType, MediaChunk | None] # type annotation
         for idx, (mid, chunks) in enumerate(av_data_loader):
             for media_type in feature_extractor_ids:
-                if media_type not in chunks:
+                if media_type not in chunks or chunks[media_type] is None:
                     continue
                 segment_tensor = chunks[media_type].tensor
                 segment_pts = chunks[media_type].pts
@@ -374,7 +374,7 @@ if __name__ == "__main__":
                         feature_metadata.id, segment_feature
                     )
 
-            if 'thumbnails' in chunks:
+            if 'thumbnails' in chunks and chunks['thumbnails'] is not None:
                 # Handle thumbnails
                 _thumb_jpegs = chunks['thumbnails'].tensor
                 _thumb_pts = chunks['thumbnails'].pts