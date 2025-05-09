058a76881d868eca6e0e9ca6295d29576c809332
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2025-05-09 23:55:06 +0100

    transformers_owlv2.py: add notice about original license as required by Apache 2
    (cherry picked from commit 4aa3afa90865cb7dc40e7da86cb9ca2f2d094733)

--- src/feature/transformers_owlv2.py
+++ src/feature/transformers_owlv2.py
@@ -1,3 +1,25 @@
+## Copyright (C) 2025 University of Oxford
+
+## Parts of the code on this file are based on the code for the
+## transformers.models.owlv2.modelling_owlv2 module from the
+## transformers Python package distribution version 4.44.2 (see
+## comments on the code below for more details).  The original code
+## had the following notice:
+##
+## Copyright 2023 Google AI and The HuggingFace Team. All rights reserved.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
 from dataclasses import dataclass
 from transformers import Owlv2Processor, Owlv2ForObjectDetection
 import torch
