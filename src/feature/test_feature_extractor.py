363e80fd529d6d141c180c0e9dba3ca6f609a2b8
Horace Lee <horacelee18ic@gmail.com>
2025-05-12 22:45:57 +0100

    Add tests for OWLv2 feature extractor

--- src/feature/test_feature_extractor.py
+++ src/feature/test_feature_extractor.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 from PIL import Image
 
-from .transformers_owlv2 import owlv2_bbox_to_xywh
+from .transformers_owlv2 import TransformersOWLv2, owlv2_bbox_to_xywh
 from .feature_extractor_factory import FeatureExtractorFactory
 
 ## Typically, imports from external libraries come before local
@@ -158,6 +158,64 @@ class TestOWLv2BBoxConversion(unittest.TestCase):
         xywh = owlv2_bbox_to_xywh(owlv2_bbox, 10, 20)
         np.testing.assert_allclose(xywh, np.array([0.1, 0.30, 0.40, 0.10]))
 
+
+class TestOWLv2FeatureExtractor(unittest.TestCase):
+    def setUp(self):
+        # use an objectness threshold of 0.0 to avoid filtering out any boxes in the output
+        self._extractor = TransformersOWLv2("transformers/owlv2/google/owlv2-base-patch16-ensemble", objectness_threshold=0.0)
+
+    def test_feature_extractor_factory_setup(self):
+        # try setting up using the feature extractor factory (just to check no errors were raised)
+        FeatureExtractorFactory("transformers/owlv2/google/owlv2-base-patch16-ensemble")
+
+    def test_text_encoder(self):
+        text_input = ['some random text']
+        text_features = self._extractor.extract_text_features(text_input)
+        self.assertTrue(text_features.shape == (1, 513))
+
+    def _preprocess_and_extract_features(self, images):
+        return self._extractor.extract_image_features(
+            self._extractor.preprocess_image(images)
+        )
+
+    def _test_images(self, images, n_images):
+        assert n_images > 0
+        features = self._preprocess_and_extract_features(images)
+        ## Check vectors
+        self.assertIsInstance(features, list)
+        self.assertEqual(len(features), n_images)
+        self.assertTrue(all([isinstance(x.vectors, np.ndarray) for x in features]))
+        self.assertTrue(all([x.vectors.shape == (3600, 513) for x in features]))
+        ## Check metadata
+        self.assertTrue(all([isinstance(x.metadata, list) for x in features]))
+        for f in features:
+            self.assertEqual(len(f.metadata), 3600)
+            self.assertTrue(all([
+                isinstance(m.objectness_score, float)
+                and isinstance(m.bbox.x, float)
+                and isinstance(m.bbox.y, float)
+                and isinstance(m.bbox.w, float)
+                and isinstance(m.bbox.h, float)
+                for m in f.metadata
+            ]))
+
+    def test_with_one_element_list(self):
+        images = [Image.new('RGB', self._extractor.input_image_size)]
+        self._test_images(images, 1)
+
+    def test_with_one_image_tensor(self):
+        input_width, input_height = self._extractor.input_image_size
+        random_image = torch.randint(0, 255, (1, 3, input_width, input_height))
+        self._test_images(random_image, 1)
+
+    def test_with_n_elements_list(self):
+        images = [Image.new('RGB', self._extractor.input_image_size) for _ in range(4)]
+        self._test_images(images, 4)
+
+    def test_with_n_images_tensor(self):
+        input_width, input_height = self._extractor.input_image_size
+        random_image = torch.randint(0, 255, (4, 3, input_width, input_height))
+        self._test_images(random_image, 4)
 
 
 if __name__ == '__main__':
