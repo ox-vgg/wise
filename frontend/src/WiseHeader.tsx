e9b681cae00bfbf352a4e789340eb038d33858ca
Abhishek Dutta <thelinuxmaniac@gmail.com>
2025-05-13 17:18:18 +0100

    search target selector dropdown now contains icons and more intelligible labels

--- frontend/src/WiseHeader.tsx
+++ frontend/src/WiseHeader.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import { Header } from 'antd/es/layout/layout';
 import { Alert, Button, Collapse, Divider, Dropdown, Flex, Form, FormInstance, Input, Popover, Select, Space, Tag, Tooltip, Upload, UploadFile, theme } from 'antd';
-import { CaretRightOutlined, CloseOutlined, FontColorsOutlined, PictureOutlined, PlusOutlined, SearchOutlined, SoundOutlined, UploadOutlined} from '@ant-design/icons';
+import { CaretRightOutlined, CloseOutlined, FontColorsOutlined, PictureOutlined, PictureTwoTone, PlusOutlined, SearchOutlined, SoundOutlined, SoundTwoTone, UploadOutlined, VideoCameraTwoTone} from '@ant-design/icons';
 import { nanoid } from 'nanoid'
 
 import './WiseHeader.scss';
@@ -394,7 +394,6 @@ const QUERY_COLORS = {
   'AUDIO_URL': 'orange'
 }
 
-/*
 const VIEW_MODALITY_OPTIONS = {
   image: {
     icon: <PictureTwoTone />,
@@ -414,8 +413,10 @@ const VIEW_MODALITY_OPTIONS = {
     longLabel: 'Audio track of video',
     value: 'VideoAudio',
   },
-}
-*/
+} as const;
+
+type ViewModalityKey = keyof typeof VIEW_MODALITY_OPTIONS;
+
 
 const WiseHeader: React.FunctionComponent<WiseHeaderProps> = ({
   multimodalQueries, setMultimodalQueries, searchText, setSearchText,
@@ -536,28 +537,38 @@ const WiseHeader: React.FunctionComponent<WiseHeaderProps> = ({
           Object.keys(projectInfo.search_targets).length > 1 &&
             <Tooltip title="Choose the media track / media type to search on">
               <Select
-              size="large"
-              variant="borderless"
-              className="wise-view-modality-select"
-              value={viewModality + ':' + featureExtractorId.split('/')[1]}
-              onChange={handleSearchTargetChange}
-              options={
-                projectInfo.search_targets
-                  ? Object.keys(projectInfo.search_targets).map((media_type) => ({
-                      label: <span>{media_type.charAt(0).toUpperCase() + media_type.slice(1)}</span>,
-                      title: media_type.charAt(0).toUpperCase() + media_type.slice(1),
-                      options: (projectInfo.search_targets?.[media_type as keyof typeof projectInfo.search_targets] || []).map((feature_extractor_id: string) => ({
-                        label: <span>{feature_extractor_id.split('/')[1]}</span>,
-                        value: media_type + ':' + feature_extractor_id,
-                      })),
-                    }))
+                size="large"
+                variant="borderless"
+                className="wise-view-modality-select"
+                value={ {'Image': 'image', 'Video': 'video', 'Audio': 'audio', 'VideoAudio': 'audio'}[viewModality] + ':' + featureExtractorId }
+                onChange={handleSearchTargetChange}
+                options={
+                  projectInfo.search_targets
+                  ? Object.keys(projectInfo.search_targets).map((media_type) => {
+                      const key = media_type as ViewModalityKey;
+                      return ({
+                        label: (
+                          <Space>
+                            {VIEW_MODALITY_OPTIONS[key]?.icon}
+                            {VIEW_MODALITY_OPTIONS[key].longLabel}
+                          </Space>
+                        ),
+                        title: media_type.charAt(0).toUpperCase() + media_type.slice(1),
+                        options: (projectInfo.search_targets?.[key] || []).map((feature_extractor_id: string) => ({
+                          ...VIEW_MODALITY_OPTIONS[key],
+                          label: (
+                            <Space>
+                              {VIEW_MODALITY_OPTIONS[key]?.icon}
+                              {feature_extractor_id.split('/')[1]}
+                            </Space>
+                          ),
+                          value: media_type + ':' + feature_extractor_id,
+                        })),
+                      })
+                    })
                   : []
-              }
-              optionRender={(option) => (
-                <Space>{option.label}</Space>
-              )}
-
-              popupMatchSelectWidth={false}
+                }
+                popupMatchSelectWidth={false}
               />
             </Tooltip>
         }
