bd40b68d57eab15d5f9ae2b537394d1e28d41b46
Prasanna Sridhar <samthedevil.sp@gmail.com>
2025-08-12 16:59:39 +0100

    update the factory to allow constructing hf / triton based feature extractors

--- src/feature/__init__.py
+++ src/feature/__init__.py
@@ -1,7 +1,12 @@
 from .feature_extractor import (
     FeatureExtractor as FeatureExtractor,
+    Features as Features,
+    BBoxXYWH as BBoxXYWH,
+    FeatureExtMetadata as FeatureExtMetadata,
+    get_torch_device as get_torch_device,
 )
-
 from .feature_extractor_factory import (
     FeatureExtractorFactory as FeatureExtractorFactory,
+    get_feature_extractor_class as get_feature_extractor_class,
+    get_canonical_feature_extractor_id as get_canonical_feature_extractor_id,
 )
--- src/feature/feature_extractor_factory.py
+++ src/feature/feature_extractor_factory.py
@@ -1,31 +1,82 @@
+
+
+import os
 import logging
 
 logger = logging.getLogger(__name__)
-# must import torch tensorrt before compile calls
+
 try:
+    # must import torch tensorrt before compile calls
     import torch_tensorrt
-except ImportError:
+except:
     logger.warning(
         "torch_tensorrt is not installed. Models will be compiled with inductor backend."
     )
     pass
 
-from .mlfoundation_openclip import MlfoundationOpenClip
-from .microsoft_clap import MicrosoftClap
-from .transformers_owlv2 import TransformersOWLv2
-from .insightface import InsightFaceFeatureExtractor
 
-def get_feature_extractor_class(id: str):
-    if id.startswith('mlfoundations/open_clip/'):
+def _get_triton_url_and_model_id(_id: str) -> tuple[str | None, str]:
+    """
+    Extracts the Triton URL and model ID from the feature extractor ID if it starts with "triton://".
+    If the ID does not start with "triton://", it returns None for url.
+    """
+    if not _id.startswith("triton://"):
+        return None, _id
+
+    model_id = _id[len("triton://") :]
+    url, model_id = model_id.split("/", 1)
+
+    return url, model_id
+
+
+def get_canonical_feature_extractor_id(_id: str) -> str:
+    """
+    Returns a canonical form of the feature extractor ID.
+    This is useful for ensuring consistent naming when using the feature
+    extractor in different contexts, such as in file names or database entries,
+    with or without triton
+    """
+    _, model_id = _get_triton_url_and_model_id(_id)
+    return model_id
+
+
+def get_triton_url_from_id(_id: str) -> str | None:
+    """
+    Extracts the Triton URL from the feature extractor ID if it starts with "triton://".
+    If the ID does not start with "triton://", it returns None.
+    """
+    url, _ = _get_triton_url_and_model_id(_id)
+    return url
+
+
+def get_feature_extractor_class(_id: str):
+
+    if _id.startswith("mlfoundations/open_clip/"):
+        from .mlfoundation_openclip import MlfoundationOpenClip
+
         return MlfoundationOpenClip
-    elif id.startswith('microsoft/clap/'):
+
+    if _id.startswith("microsoft/clap/"):
+        from .microsoft_clap import MicrosoftClap
+
         return MicrosoftClap
-    elif id.startswith('transformers/owlv2/'):
+
+    if _id.startswith("transformers/owlv2/"):
+        from .transformers_owlv2 import TransformersOWLv2
+
         return TransformersOWLv2
-    elif id.startswith('deepinsight/insightface/'):
+
+    if _id.startswith("deepinsight/insightface/"):
+        from .insightface import InsightFaceFeatureExtractor
+
         return InsightFaceFeatureExtractor
-    else:
-        raise ValueError(f'Unknown feature extractor id {id}')
+
+    if _id.startswith("hf/"):
+        from .hf_feature_extractor import HFMultiModalFeatureExtractor
+
+        return HFMultiModalFeatureExtractor
+
+    raise ValueError(f"Unknown feature extractor id {_id}")
 
 
 def FeatureExtractorFactory(id, **kwargs):
@@ -60,12 +111,25 @@ def FeatureExtractorFactory(id, **kwargs):
         - if both REPOSITORY_NAME and NAME_NAME are same (e.g. CLAP), use model release year
             (e.g. microsoft/clap/2023/four-datasets/)
     """
-    if len(id.split('/')) != 4:
+    url, model_id = _get_triton_url_and_model_id(id)
+    is_triton = url is not None
+
+    if len(model_id.split("/")) != 4:
         raise ValueError('''Feature extractor name must be formatted as
               USER_OR_ORGANIZATION / REPOSITORY_NAME / MODEL_NAME / TRAINING_DATASET
             For example, use "mlfoundations/open_clip/ViT-B-16-SigLIP-256/webli" for extracting features using ViT
             model trained on the Web Language Image (WebLI) dataset.
             ''')
-    
-    cls = get_feature_extractor_class(id)
-    return cls(id, **kwargs)
+
+    cls = get_feature_extractor_class(model_id)
+    if is_triton:
+        from .triton_runner import make_triton_feature_extractor
+
+        # TODO move this to pydantic settings and inject it
+        url = url or os.environ.get("WISE_TRITON_URL", "localhost:8001")
+        logger.info(
+            f"Creating Triton-enabled feature extractor for {model_id} at {url}"
+        )
+        cls = make_triton_feature_extractor(cls, url=url)
+
+    return cls(model_id, **kwargs)
