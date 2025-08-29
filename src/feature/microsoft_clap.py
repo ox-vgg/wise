19bef6193341a0ed96df02f15cf0beeabadfd08e
Prasanna Sridhar <samthedevil.sp@gmail.com>
2025-08-29 11:26:54 +0100

    copy preprocessing function from CLAP project into WISE and create a static method to make CLAP compatible with dataloader that uses workers in 'spawn' mode

--- src/feature/microsoft_clap.py
+++ src/feature/microsoft_clap.py
@@ -1,5 +1,7 @@
+import collections
 from functools import cached_property
 import logging
+import re
 from msclap import CLAP
 import torch
 import numpy as np
@@ -9,6 +11,62 @@ from collections.abc import Iterable
 from .feature_extractor import FeatureExtractor, get_torch_device
 
 logger = logging.getLogger(__name__)
+
+# Copied from clap to make it pickleable
+np_str_obj_array_pattern = re.compile(r"[SaUO]")
+default_collate_err_msg_format = (
+    "default_collate: batch must contain tensors, numpy arrays, numbers, "
+    "dicts or lists; found {}"
+)
+
+
+def default_collate(batch):
+    r"""Puts each data field into a tensor with outer dimension batch size"""
+    elem = batch[0]
+    elem_type = type(elem)
+    if isinstance(elem, torch.Tensor):
+        out = None
+        if torch.utils.data.get_worker_info() is not None:
+            # If we're in a background process, concatenate directly into a
+            # shared memory tensor to avoid an extra copy
+            numel = sum([x.numel() for x in batch])
+            storage = elem.storage()._new_shared(numel)
+            out = elem.new(storage)
+        return torch.stack(batch, 0, out=out)
+    elif (
+        elem_type.__module__ == "numpy"
+        and elem_type.__name__ != "str_"
+        and elem_type.__name__ != "string_"
+    ):
+        if elem_type.__name__ == "ndarray" or elem_type.__name__ == "memmap":
+            # array of string classes and object
+            if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+            return default_collate([torch.as_tensor(b) for b in batch])
+        elif elem.shape == ():  # scalars
+            return torch.as_tensor(batch)
+    elif isinstance(elem, float):
+        return torch.tensor(batch, dtype=torch.float64)
+    elif isinstance(elem, int):
+        return torch.tensor(batch)
+    elif isinstance(elem, str):
+        return batch
+    elif isinstance(elem, collections.abc.Mapping):
+        return {key: default_collate([d[key] for d in batch]) for key in elem}
+    elif isinstance(elem, tuple) and hasattr(elem, "_fields"):  # namedtuple
+        return elem_type(*(default_collate(samples) for samples in zip(*batch)))
+    elif isinstance(elem, collections.abc.Sequence):
+        # check to make sure that the elements in batch have consistent size
+        it = iter(batch)
+        elem_size = len(next(it))
+        if not all(len(elem) == elem_size for elem in it):
+            raise RuntimeError("each element in list of batch should be of equal size")
+        transposed = zip(*batch)
+        return [default_collate(samples) for samples in transposed]
+
+    raise TypeError(default_collate_err_msg_format.format(elem_type))
+
 
 class MicrosoftClap(FeatureExtractor):
     """
@@ -47,14 +105,15 @@ class MicrosoftClap(FeatureExtractor):
         logger.info(f'Initialising model {self.ID_PREFIX} ({self.version}, use_cuda={use_cuda})')
         return CLAP(version=self.version, use_cuda=use_cuda)
 
-    def preprocess_audio(self, audio: torch.Tensor) -> torch.Tensor:
+    @staticmethod
+    def preprocess_audio(audio: torch.Tensor) -> torch.Tensor:
         # CLAP accepts (1xN_samples)
         if audio.shape[0] > 2:
             audio = torch.transpose(audio, 0, 1)
         # the CLAP model only accepts single channel audio
         if audio.shape[0] != 1:
             audio = torch.mean(audio, 0, keepdim=True)
-        return self.model.default_collate([audio])
+        return default_collate([audio])
 
     def preprocess_text(self, text: str) -> str:
         return self.model.preprocess_text(text)
