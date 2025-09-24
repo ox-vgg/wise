728993fd6c6d1f1f21f3f2d88f27a9580821d4f4
Prasanna Sridhar <samthedevil.sp@gmail.com>
2025-09-24 09:04:09 +0100

    encapsulate feature extraction into embedding service

---
+++ api/services/embedding/__init__.py
@@ -0,0 +1,6 @@
+from ._embedding import EmbeddingConfig, EmbeddingService
+from .exceptions import (
+    ModalityNotSupportedError,
+    FeatureExtractorNotFoundError,
+    NoFeaturesFoundError,
+)
