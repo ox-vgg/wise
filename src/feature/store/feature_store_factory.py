d2a9eca61b8f4274c0fcd70bced316aa4112134d
David Miguel Susano Pinto <pinto@robots.ox.ac.uk>
2026-02-06 14:31:40 +0000

    FeatureStoreFactory: use Modality.value to get store filename (#209)
    ModalityType is an enum and not a string so use the enum value to get
    a string which can be used for filenames.  Issue #209 happens because
    Python 3.11 changed the string representation of enums.  By using the
    enum value (str) instead of the enum, we avoid the issue.

--- src/feature/store/feature_store_factory.py
+++ src/feature/store/feature_store_factory.py
@@ -18,31 +18,39 @@ from pathlib import Path
 import enum
 
 from .faiss_store import FaissStore
+from ...data_models import ModalityType
 
 class FeatureStoreType(str, enum.Enum):
     FAISS = "faiss"
 
 class FeatureStoreFactory:
     @classmethod
-    def create_store(cls, feature_store_type: FeatureStoreType, modality_type, features_dir):
+    def create_store(
+        cls,
+        feature_store_type: FeatureStoreType,
+        modality_type: ModalityType,
+        features_dir,
+    ):
+        store_name_prefix = modality_type.value
         if feature_store_type == FeatureStoreType.FAISS:
-            return FaissStore(modality_type, features_dir)
+            return FaissStore(store_name_prefix, features_dir)
         else:
             raise ValueError(f'unknown feature_store_type {feature_store_type}')
 
     @classmethod
-    def load_store(cls, modality_type, features_dir):
+    def load_store(cls, modality_type: ModalityType, features_dir):
         features_dir = Path(features_dir) # convert type in case features_dir is a string
+        store_name_prefix = modality_type.value
 
         # infer the store type
-        shard_suffixes = set([p.suffix for p in features_dir.glob(modality_type + '-*')])
+        shard_suffixes = set([p.suffix for p in features_dir.glob(store_name_prefix + '-*')])
         if len(shard_suffixes) == 0:
-            raise ValueError(f'found no feature store files in {features_dir} for type {modality_type}')
+            raise ValueError(f"found no feature store files '{features_dir}{store_name_prefix}-*'")
         elif len(shard_suffixes) > 1:
-            raise ValueError(f'failed to infer type of {modality_type} feature store in {features_dir} because there are multiple file types present ({shard_suffixes})')
+            raise ValueError(f"failed to infer type of '{features_dir}/{store_name_prefix}-*' feature store files because there are multiple file types present ({shard_suffixes})")
 
         shard_suffix = shard_suffixes.pop()
         if shard_suffix == ".faiss":
-            return FaissStore(modality_type, features_dir)
+            return FaissStore(store_name_prefix, features_dir)
         else:
             raise ValueError(f'unknown store containing shard filenames with extension {shard_suffix}')
