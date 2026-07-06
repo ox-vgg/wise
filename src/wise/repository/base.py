7e5280cf3dc393fbb8c6b75401d2c64dc3beb053
David Miguel Susano Pinto <pinto@robots.ox.ac.uk>
2026-07-06 17:56:14 +0100

    Repository: update ABC abstract methods signature to SQLAlchemyRepository
    We only have the SQLAlchemyRepository implementation for Repository
    (maybe we shouldn't be using an ABC here) and the actual abstract
    methods signature has already diverged from the ABC.  This commit
    changes the ABC to match the reality of the only concrete class.

--- src/wise/repository/base.py
+++ src/wise/repository/base.py
@@ -32,27 +32,29 @@ class EntityNotFoundException(Exception):
 
 class Repository(ABC, Generic[Entity, EntityCreate, EntityUpdate]):
     @abstractmethod
-    def get(self, id: Any) -> Optional[Entity]:
+    def get(self, conn: sa.Connection, id: Any) -> Optional[Entity]:
         raise NotImplementedError
 
     @abstractmethod
-    def list(self) -> Iterable[Entity]:
+    def list(self, conn: sa.Connection) -> Iterable[Entity]:
         raise NotImplementedError
 
     @abstractmethod
-    def create(self, obj: EntityCreate):
+    def create(self, conn: sa.Connection, *, data: EntityCreate):
         raise NotImplementedError
 
     @abstractmethod
-    def update(self, id: Any, obj: EntityUpdate) -> Entity:
+    def update(
+        self, conn: sa.Connection, id: Any, *, data: EntityUpdate
+    ) -> Entity:
         raise NotImplementedError
 
     @abstractmethod
-    def delete(self, id: Any) -> Entity:
+    def delete(self, conn: sa.Connection, id: Any) -> Entity:
         raise NotImplementedError
 
     @abstractmethod
-    def delete_all(self):
+    def delete_all(self, conn: sa.Connection):
         raise NotImplementedError
 
 
