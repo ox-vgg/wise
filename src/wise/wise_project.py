6ff54a062a0ac196076349211260c6598bef4a75
David Miguel Susano Pinto <pinto@robots.ox.ac.uk>
2026-07-06 19:23:46 +0100

    WiseProject: always use URI format to open sqlite databases
    Despite the methods names 'dburi' and 'thumbs_uri' we actually only
    use URIs when in read_only.  For consistency, always use URIs.

--- src/wise/wise_project.py
+++ src/wise/wise_project.py
@@ -183,10 +183,10 @@ class WiseProject:
         return self.project_dir.name
 
     def _dbpath_to_uri(self, dbpath: Path) -> str:
-        abs_dbpath = dbpath.absolute()
-        if self.read_only:
-            return f"{DB_SCHEME}/{abs_dbpath.as_uri()}?mode=ro&uri=true"
-        return f"{DB_SCHEME}/{abs_dbpath.as_uri()}"
+        ## XXX: We have been using "rwc" (implicitly) but maybe we
+        ## should use "rw" and only "rwc" when create_project is true.
+        mode = "ro" if self.read_only else "rwc"
+        return f"{DB_SCHEME}/{dbpath.absolute.as_uri()}?mode={mode}&uri=true"
 
     @property
     def thumbs_uri(self) -> str:
