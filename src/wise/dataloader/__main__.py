f90d4d2fed67453e198c7d8a2cb15a2b8326b1f0
David Miguel Susano Pinto <pinto@robots.ox.ac.uk>
2026-07-22 12:17:25 +0100

    dataloader._extra: move into dataloader.__main__ since it is only used there

--- src/wise/dataloader/__main__.py
+++ src/wise/dataloader/__main__.py
@@ -14,26 +14,102 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
+import enum
 import functools
 import itertools
 import logging
+from collections.abc import Callable
 from pathlib import Path
 from typing import Optional
 
+import numpy as np
+import open_clip
 import torch
 import torch.utils.data as torch_data
+import torchvision.transforms.v2 as transforms_v2
 import typer
 from tqdm import tqdm
 
-from wise.dataloader._extra import (
-    CLIPModel,
-    _preprocess,
-    get_input_transform_for_model,
-)
 from wise.dataloader.dataset import get_dataset, get_metadata_for_valid_files
 from wise.dataloader.utils import get_files_from_directory_with_extensions
 
 logger = logging.getLogger(__name__)
+
+
+AVAILABLE_MODELS = open_clip.list_pretrained(as_str=True) + ["internvideo"]
+
+INTERNVIDEO_MEAN = np.array(
+    [0.48145466, 0.4578275, 0.40821073], dtype=np.float32
+)
+INTERNVIDEO_STD = np.array(
+    [0.26862954, 0.26130258, 0.27577711], dtype=np.float32
+)
+
+
+def squeeze_(x):
+    return x.squeeze(0)
+
+
+def unsqueeze_(x):
+    return x.unsqueeze(0)
+
+
+def permute_(x):
+    return x.permute(1, 0, 2, 3)
+
+
+def get_input_transform_for_model(clip_model):
+    if clip_model == "internvideo":
+        # Internvideo preprocessing
+        return transforms_v2.Compose(
+            [
+                # Convert chunk to tensor to overcome issue with
+                # .numpy() method in tensor subclasses
+                transforms_v2.Resize(224),
+                transforms_v2.CenterCrop(224),
+                transforms_v2.ToDtype(torch.float32, scale=True),
+                transforms_v2.Normalize(
+                    mean=INTERNVIDEO_MEAN.tolist(),
+                    std=INTERNVIDEO_STD.tolist(),
+                ),
+                # C x B x H x W
+                permute_,
+            ]
+        )
+
+    model_name, _ = clip_model.value.split(":", 1)
+    logger.info("Loading CLIP (model: %s)...", model_name)
+    model = open_clip.create_model(model_name, None)
+    preprocess = open_clip.transform.image_transform_v2(
+        open_clip.transform.PreprocessCfg(**model.visual.preprocess_cfg),
+        is_train=False,
+    )
+
+    return transforms_v2.Compose(
+        [
+            # Convert chunk to tensor to overcome issue with
+            # .numpy() method in tensor subclasses
+            squeeze_,
+            transforms_v2.ToPILImage(),
+            preprocess,
+            unsqueeze_,
+        ]
+    )
+
+
+class _CLIPModel(str, enum.Enum):
+    pass
+
+
+CLIPModel = _CLIPModel(
+    "CLIPModel", {x: x for x in AVAILABLE_MODELS} | {"None": None}
+)
+
+
+def _preprocess(
+    preprocess_fn: Callable, x: torch.Tensor
+) -> Callable[[torch.Tensor], torch.Tensor]:
+    return torch.stack([preprocess_fn(xi) for xi in x])
 
 
 if __name__ == "__main__":
